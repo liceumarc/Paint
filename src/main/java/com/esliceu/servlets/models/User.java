c93f698f4b293d6a833c8af8791a5f497eb3a436
Marc <mmorellw@esliceu.net>
2025-10-29 03:45:09 +0100

    login + register

--- src/main/java/com/esliceu/servlets/models/User.java
+++ src/main/java/com/esliceu/servlets/models/User.java
@@ -1,4 +1,42 @@
 package com.esliceu.servlets.models;
 
 public class User {
+
+    private String login;
+    private String name;
+    private String password;
+
+    public User(String login, String password, String name) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean checkPassword(String providedPassword) {
+        return this.password.equals(providedPassword);
+    }
 }
